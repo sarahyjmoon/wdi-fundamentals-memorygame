02c5e6ae0b1b21a2aa978524ad7a2bbb2ec734f8
Moon <Sarah.Y.Moon@disney.com>
2018-01-13 19:09:59 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,3 @@
-console.log("Up and running!");
-
 /* Removed previous variables
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree); */
@@ -7,20 +5,21 @@ console.log("User flipped " + cardThree); */
 // card values
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay [1]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay [1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 }
+var flipCard = function(cardId) {
+  console.log("Up and running!");
+
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
